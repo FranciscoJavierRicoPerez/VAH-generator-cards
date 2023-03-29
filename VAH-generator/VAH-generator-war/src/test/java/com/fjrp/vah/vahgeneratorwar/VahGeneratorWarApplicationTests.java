012b8009e9e156d1cb72244015a15398203bbf24
DESKTOP-85GO8CH\RiCo9 <franricoperez1997@gmail.com>
2023-03-29 22:24:54 +0200

    Initial commit

---
+++ VAH-generator/VAH-generator-war/src/test/java/com/fjrp/vah/vahgeneratorwar/VahGeneratorWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjrp.vah.vahgeneratorwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VahGeneratorWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
